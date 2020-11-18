e0944e76ca72e6223d70658aa974632fc9c3ed01
sdcoffey <stevendcoffey@gmail.com>
2020-11-17 18:02:38 -0800

    export validator types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { getDefaultSchema, loadSchemaFile, setMocks } from "./load-schema";
 export { GraphQLExecutionError } from "./GraphQLExecutionError";
 export { mockQueryResponse } from "./mock";
 export { mockUseQuery, mockUseMutation, restoreMocks } from "./apollo-mocks";
+export { MutationValidator, QueryValidator } from "./validators";
